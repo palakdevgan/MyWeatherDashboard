f6963cec4a645082b9c3443a933d31ed93753e42
palakdevgan <m7.palak@gmail.com>
2022-03-29 10:56:46 -0400

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var city="Toronto";
-
+var currentH1El=document.querySelector("#currentcity");
+var currentImgEl=document.querySelector("#currentimg");
 var getCoordinates = function(city){
 
 var apiUrl="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=36cf548e4720f5cb0d8e91c0678423fe";
@@ -7,7 +8,7 @@ var apiUrl="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=36cf54
 fetch(apiUrl).then(function(response){
     if(response.ok){
         response.json().then(function(data){
-            getWeatherData(data.lat,data.lon);
+            getWeatherData(data[0].lat,data[0].lon);
         });
     }
     else{
@@ -17,12 +18,15 @@ fetch(apiUrl).then(function(response){
 };
 
 var getWeatherData = function(lat,lon){
-var apiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"exclude=hourly,minutely&units=imperial&appid=36cf548e4720f5cb0d8e91c0678423fe";   
+var apiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,minutely&units=imperial&appid=36cf548e4720f5cb0d8e91c0678423fe";   
 
 fetch(apiUrl).then(function(response){
     if(response.ok){
         response.json().then(function(data){
-                  console.log(data);
+                  var today = moment.unix(data.current.dt).format("MM/DD/YYYY");
+                  currentH1El.textContent=data.timezone+" ("+today+")";
+                  currentImgEl.setAttribute("src","http://openweathermap.org/img/wn/10d.png");
+                  currentImgEl.setAttribute("style","width=50px height=50px");
         });
     }
     else{
