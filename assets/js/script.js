6630efd5ad622aee52c73a5936843b908d12b0a6
palakdevgan <m7.palak@gmail.com>
2022-03-29 11:34:19 -0400

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var city="Toronto";
 var currentH1El=document.querySelector("#currentcity");
 var currentImgEl=document.querySelector("#currentimg");
+var currentTempEl=document.querySelector("#currenttemp");
+var currentWindEl=document.querySelector("#currentwind");
+var currentTitleEl=document.querySelector("#currenttitle");
 var getCoordinates = function(city){
 
 var apiUrl="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=36cf548e4720f5cb0d8e91c0678423fe";
@@ -26,7 +29,10 @@ fetch(apiUrl).then(function(response){
                   var today = moment.unix(data.current.dt).format("MM/DD/YYYY");
                   currentH1El.textContent=data.timezone+" ("+today+")";
                   currentImgEl.setAttribute("src","http://openweathermap.org/img/wn/10d.png");
-                  currentImgEl.setAttribute("style","width=50px height=50px");
+                  currentImgEl.setAttribute("style","width=50px;height=50px");
+                  currentTitleEl.setAttribute("style","display:flex");
+                  currentTempEl.innerHTML="Temp: "+data.current.temp+"&#8457";
+                  currentWindEl.textContent="Wind: "+data.current.wind_speed+" MPH";
         });
     }
     else{
